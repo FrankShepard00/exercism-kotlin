380f54f3d757e6a85208bf433ae8ace9eaf10a98
FrankShepard00 <136857218+FrankShepard00@users.noreply.github.com>
2026-02-11 14:50:30 -0600

    Bob

--- kotlin/bob/src/main/kotlin/Bob.kt
+++ kotlin/bob/src/main/kotlin/Bob.kt
@@ -1,20 +1,17 @@
 object Bob {
 
-    private fun String.isSilence() = isEmpty()
-    private fun String.isQuestion() = endsWith("?")
-    private fun String.hasLetters() = any { it.isLetter() }
-    private fun String.isAllCaps() = hasLetters() && uppercase() == this
-    private fun String.isYelling() = isAllCaps()
-    private fun String.isYellingQuestion() = isYelling() && isQuestion()
+    fun String.isQuestion(): Boolean = endsWith("?")
+    fun String.isYelling(): Boolean = all { !it.isLetter() || it.isUpperCase() } && any { it.isLetter() }
+    fun String.isYellingQuestion(): Boolean = isYelling() && isQuestion()
+    fun String.isSilence(): Boolean = isBlank()
 
     fun hey(input: String): String {
-        val cleaned = input.trim()
-
+        val cleanPeopleSay = input.trim()
         return when {
-            cleaned.isSilence() -> "Fine. Be that way!"
-            cleaned.isYellingQuestion() -> "Calm down, I know what I'm doing!"
-            cleaned.isYelling() -> "Whoa, chill out!"
-            cleaned.isQuestion() -> "Sure."
+            cleanPeopleSay.isSilence() -> "Fine. Be that way!"
+            cleanPeopleSay.isYellingQuestion() -> "Calm down, I know what I'm doing!"
+            cleanPeopleSay.isYelling() -> "Whoa, chill out!"
+            cleanPeopleSay.isQuestion() -> "Sure."
             else -> "Whatever."
         }
     }
